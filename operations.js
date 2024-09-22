195516291cafc4a1e8450cec0538023cb57caefa
Cauan <cauanbarros14@gmail.com>
2024-09-21 22:49:19 -0300

    now yes

---
+++ operations.js
@@ -0,0 +1,19 @@
+// Soma
+console.log(10 + 22); // 32
+
+// Subtração
+console.log(100 - 40); // 60
+
+// Multiplicação
+console.log(4 * 7); // 28
+
+// Divisão
+console.log(40 / 5); // 8
+
+console.log( 5 % 2)
+
+let forIncrement = 1
+console.log(forIncrement += 4)
+
+let forDecrement = 5
+console.log(forDecrement -= 1)
