40c72079fa07825d40a28bf537256829aa5292b3
Cauan <cauanbarros14@gmail.com>
2024-09-21 23:21:41 -0300

    comparators

---
+++ first-steps/operations-conditionals.js
@@ -0,0 +1,14 @@
+
+const agePeople = 18 
+const biggerOfAge = 18
+
+const haveMoneyForDrink = false;
+
+if(agePeople >= biggerOfAge && haveMoneyForDrink == true) {
+    console.log("Lets go we fun")
+    } else if (agePeople < biggerOfAge) {
+        console.log("you not can if fun")
+    }
+    else {
+        console.log("you not have money for fun")
+    }
